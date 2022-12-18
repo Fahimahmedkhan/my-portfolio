00464fb3e9d764838aef4469d0b9a91167ee25fc
Fahim Ahmed Khan Gorbo <rolx9999@gmail.com>
2022-12-18 20:57:33 +0600

    Update ResumeSection.js

--- src/Pages/Home/ResumeSection/ResumeSection.js
+++ src/Pages/Home/ResumeSection/ResumeSection.js
@@ -22,7 +22,7 @@ const ResumeSection = () => {
                     <div className="divider-horizontal md:divider-horizontal lg:divider-horizontal"></div>
                     <div className="grid flex-grow card rounded-lg  bg-base-300 py-10 px-10 ">
                         <div className="">
-                            <span className="text-yellow-500 font-semibold text-4xl">2020</span>
+                            <span className="text-yellow-500 font-semibold text-4xl">2022</span>
                             <h2 className='text-2xl text-gray-500 py-2 px-2'>Complete Web Development Course With Jhankar Mahbub</h2>
                             <span className="text-xl text-gray-500 px-2">Programming Hero</span>
                             <br /> <br />
